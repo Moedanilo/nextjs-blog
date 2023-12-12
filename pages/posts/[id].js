c1ee8539d40841052e7dc1b5b1c584a5ab8b2316
Mo2 . <moses.uriarte1@gmail.com>
2023-12-12 14:48:35 -0500

    Polished by adding date to post, title, and css link

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,10 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '/lib/posts';
+// Add this import
+import Head from 'next/head';
+// Add this import
+import Date from '../../components/date';// Add this import at the top of the file
+import utilStyles from '../../styles/utils.module.css';
 
 export async function getStaticProps({ params }) {
     // Add the "await" keyword like this:
@@ -20,16 +25,19 @@ export async function getStaticPaths() {
   };
 }
 
-export default function Post({ postData }) {
+  export default function Post({ postData }) {
     return (
       <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-        <br />
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
+        <article>
+          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+          <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
       </Layout>
     );
   }
